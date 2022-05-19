fe5970a8748a17249a2edf66af56e3fb6426b862
fanjc320 <894020798@qq.com>
2022-05-19 14:52:14 +0800

    加上我自定义的方法+对比更清晰

--- chapter02/ten_armed_testbed.py
+++ chapter02/ten_armed_testbed.py
@@ -12,20 +12,30 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import trange
+import logging
 
 matplotlib.use('Agg')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+# formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+# file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+with open('logs.log', 'w'):
+    pass
 
 class Bandit:
     # @k_arm: # of arms
     # @epsilon: probability for exploration in epsilon-greedy algorithm
     # @initial: initial estimation for each action
-    # @step_size: constant step size for updating estimations
     # @sample_averages: if True, use sample averages to update estimations instead of constant step size
+    # @step_size: constant step size for updating estimations
     # @UCB_param: if not None, use UCB algorithm to select action
     # @gradient: if True, use gradient based bandit algorithm
     # @gradient_baseline: if True, use average reward as baseline for gradient based bandit algorithm
-    def __init__(self, k_arm=5, epsilon=0., initial=0., step_size=0.1, sample_averages=False, UCB_param=None,
+    def __init__(self, k_arm=4, epsilon=0., initial=0., step_size=0.1, sample_averages=False, UCB_param=None,
                  gradient=False, gradient_baseline=False, true_reward=0.):
         self.k = k_arm
         self.step_size = step_size
@@ -43,9 +53,14 @@ class Bandit:
     def reset(self):
         # real reward for each action
         self.q_true = np.random.randn(self.k) + self.true_reward
-
+        logger.info("self.q_true:")
+        self.q_true = np.array([0,-1,5,0.5])
+        logger.info(self.q_true)
+        logger.info("self.q_true end")
         # estimation for each action
         self.q_estimation = np.zeros(self.k) + self.initial
+        self.q_estimation_fjc = np.copy(self.q_estimation)
+        self.all_reward = np.zeros(self.k) #fjc
 
         # # of chosen times for each action
         self.action_count = np.zeros(self.k)
@@ -57,7 +72,8 @@ class Bandit:
     # get an action for this bandit
     def act(self):
         if np.random.rand() < self.epsilon:
-            return np.random.choice(self.indices)
+            rnd = np.random.choice(self.indices)
+            return rnd, rnd
 
         if self.UCB_param is not None:
             UCB_estimation = self.q_estimation + \
@@ -70,22 +86,33 @@ class Bandit:
             self.action_prob = exp_est / np.sum(exp_est)
             return np.random.choice(self.indices, p=self.action_prob)
 
+        # res = 0
+        # res_fjc = 0
+        # if fjc:
+        q_best_fjc = np.max(self.q_estimation_fjc)
+        tmp_fjc = np.where(self.q_estimation_fjc == q_best_fjc)[0]
+        act_fjc = np.random.choice(tmp_fjc)
+        # else:
         q_best = np.max(self.q_estimation)
         tmp = np.where(self.q_estimation == q_best)[0]
-        res = np.random.choice(tmp)
-        return res
+        act = np.random.choice(tmp)
+        return act, act_fjc
 
     # take an action, update estimation for this action
-    def step(self, action):
+    def step(self, action, fjc = False):
         # generate the reward under N(real reward, 1)
         reward = np.random.randn() + self.q_true[action]
+        self.all_reward[action] += reward
         self.time += 1
         self.action_count[action] += 1
         self.average_reward += (reward - self.average_reward) / self.time
 
         if self.sample_averages:
             # update estimation using sample averages
-            self.q_estimation[action] += (reward - self.q_estimation[action]) / self.action_count[action]
+            if fjc:
+                self.q_estimation_fjc[action] += (self.all_reward[action]) / self.action_count[action]
+            else:
+                self.q_estimation[action] += (reward - self.q_estimation[action]) / self.action_count[action]
         elif self.gradient:
             one_hot = np.zeros(self.k)
             one_hot[action] = 1
@@ -97,24 +124,38 @@ class Bandit:
         else:
             # update estimation with constant step size
             self.q_estimation[action] += self.step_size * (reward - self.q_estimation[action])
+        # logger.info("--------------------------------fjc:",fjc)
+        # logger.info(self.q_estimation)
         return reward
 
 
 def simulate(runs, time, bandits):
     rewards = np.zeros((len(bandits), runs, time))
+    rewards_fjc = np.zeros((len(bandits), runs, time))
     best_action_counts = np.zeros(rewards.shape)
+    best_action_counts_fjc = np.zeros(rewards_fjc.shape)
     for i, bandit in enumerate(bandits):
         for r in trange(runs):
             bandit.reset()
             for t in range(time):
-                action = bandit.act()
+                action,action_fjc = bandit.act()
                 reward = bandit.step(action)
+                reward_fjc = bandit.step(action_fjc,fjc=True)
                 rewards[i, r, t] = reward
+                rewards_fjc[i, r, t] = reward_fjc
                 if action == bandit.best_action:
                     best_action_counts[i, r, t] = 1
+
+                if action_fjc == bandit.best_action:
+                    best_action_counts_fjc[i, r, t] = 1
+
     mean_best_action_counts = best_action_counts.mean(axis=1)
     mean_rewards = rewards.mean(axis=1)
-    return mean_best_action_counts, mean_rewards
+
+    mean_best_action_counts_fjc = best_action_counts_fjc.mean(axis=1)
+    mean_rewards_fjc = rewards_fjc.mean(axis=1)
+
+    return mean_best_action_counts, mean_rewards, mean_best_action_counts_fjc, mean_rewards_fjc
 
 
 def figure_2_1():
@@ -135,26 +176,52 @@ def figure_2_1():
     plt.close()
 
 
-def figure_2_2(runs=2000, time=1000):
-    epsilons = [0, 0.1, 0.01]
+def figure_2_2(runs=20, time=100):
+    # epsilons = [0, 0.1, 0.01]
+    epsilons = [0.1]
     bandits = [Bandit(epsilon=eps, sample_averages=True) for eps in epsilons]
-    best_action_counts, rewards = simulate(runs, time, bandits)
+    best_action_counts, rewards, best_action_counts_fjc, rewards_fjc = simulate(runs, time, bandits)
 
-    plt.figure(figsize=(10, 20))
+    plt.figure(figsize=(20, 20))
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(2, 2, 1)
     for eps, rewards in zip(epsilons, rewards):
         plt.plot(rewards, label='$\epsilon = %.02f$' % (eps))
+    for eps, rewards in zip(epsilons, rewards_fjc):
+        plt.plot(rewards, label='$\_epsilon_fjc = %.02f$' % (eps))
     plt.xlabel('steps')
     plt.ylabel('average reward')
     plt.legend()
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(2, 2, 2)
     for eps, counts in zip(epsilons, best_action_counts):
+        plt.plot(counts, label='$\epsilon = %.02f$' % (eps))
+    for eps, counts in zip(epsilons, best_action_counts_fjc):
         plt.plot(counts, label='$\epsilon = %.02f$' % (eps))
     plt.xlabel('steps')
     plt.ylabel('% optimal action')
     plt.legend()
+
+    #-----------------------------------------------------------
+    logger.info("****************************   fjc   *********************************")
+    # bandits = [Bandit(epsilon=eps, sample_averages=True,fjc=True) for eps in epsilons]
+    # best_action_counts, rewards = simulate(runs, time, bandits)
+
+    plt.subplot(2, 2, 3)
+    for eps, rewards in zip(epsilons, rewards_fjc):
+        plt.plot(rewards, label='$\epsilon = %.02f$' % (eps))
+    plt.xlabel('steps fjc')
+    plt.ylabel('average reward fjc')
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    for eps, counts in zip(epsilons, best_action_counts_fjc):
+        plt.plot(counts, label='$\epsilon = %.02f$' % (eps))
+    plt.xlabel('steps fjc')
+    plt.ylabel('% optimal action fjc')
+    plt.legend()
+
+
 
     plt.savefig('../images/figure_2_2.png')
     plt.close()
@@ -248,9 +315,9 @@ def figure_2_6(runs=2000, time=1000):
 
 
 if __name__ == '__main__':
-    figure_2_1()
+    # figure_2_1()
     figure_2_2()
-    figure_2_3()
-    figure_2_4()
-    figure_2_5()
-    figure_2_6()
+    # figure_2_3()
+    # figure_2_4()
+    # figure_2_5()
+    # figure_2_6()
